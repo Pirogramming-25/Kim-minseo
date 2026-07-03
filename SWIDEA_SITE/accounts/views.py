c7b03dd7c878ebdfec6db0566721fa7a812f9ca8
minsxo <kimminseo4216@gmail.com>
2026-07-04 02:36:33 +0900

    feat: 회원가입과 로그인 기능 구현

---
+++ SWIDEA_SITE/accounts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+from ideas.models import IdeaStar
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("accounts:mypage")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/signup.html", {"form": form})
+
+
+@login_required
+def mypage(request):
+    starred_ideas = (
+        IdeaStar.objects.filter(user=request.user)
+        .select_related("idea", "idea__devtool")
+        .order_by("-created_at")
+    )
+    return render(request, "accounts/mypage.html", {"starred_ideas": starred_ideas})
